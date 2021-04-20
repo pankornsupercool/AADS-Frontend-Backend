651806a2125024b5555cda7897ad9eb08a71ffda
teerapop67 <52346089+teerapop67@users.noreply.github.com>
2021-04-20 20:25:46 +0700

    update MarkersDetail

---
+++ AADS/MarkersDetail/VitalAsset.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AADS.MarkersDetail
+{
+    class VitalAsset
+    {
+        public static string VitalAssetID;
+        public static string VitalAssetName;
+        public static string VitalAssetType;
+        public static string VitalAssetPriority;
+        public static string VitalAssetAreaName;
+        public static string VitalAssetProvince;
+        public static string VitalAssetAssetSide;
+        public static string VitalAssetUnitResponsible;
+        public static string VitalAssetUnitStatus;
+        public static string VitalAssetSimStatus;
+
+
+    }
+}
